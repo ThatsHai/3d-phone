393c4efc5658d30437cad3c408e434902a82b781
Hai Nguyen <nguyendonghaiwork94@gmail.com>
2024-07-30 16:22:52 +0700

    Finishing touch

--- phone/.eslintrc.cjs
+++ phone/.eslintrc.cjs
@@ -10,9 +10,10 @@ module.exports = {
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
   settings: { react: { version: '18.2' } },
-  plugins: ['react-refresh'],
+  plugins: ['react-refresh', '@react-three'],
   rules: {
     'react/jsx-no-target-blank': 'off',
+    
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
--- phone/src/App.jsx
+++ phone/src/App.jsx
@@ -1,18 +1,25 @@
-import Navbar from './components/Navbar'
-import Hero from './components/Hero'
-import Highlights from './components/Highlight'
-import './App.css'
+import Navbar from './components/Navbar';
+import Hero from './components/Hero';
+import Highlights from './components/Highlights';
+import Model from './components/Model';
+import Features from './components/Features.jsx';
+import HowItWorks from './components/HowItWork';
+import Footer from './components/Footer';
 
-function App() {
-  
-  
+import * as Sentry from '@sentry/react';
+
+const App = () => {
   return (
     <main className="bg-black">
       <Navbar />
       <Hero />
       <Highlights />
+      <Model />
+      <Features />
+      <HowItWorks />
+      <Footer />
     </main>
   )
 }
 
-export default App
+export default Sentry.withProfiler(App);
